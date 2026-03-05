498b54e94e961cf68197c1fd260d2a3ed384864c
Dilaray Mercan <dilaray.cam@gmail.com>
2026-03-05 23:25:38 +0300

    login ve success sayfaları eklendi

---
+++ src/components/Login.jsx
@@ -0,0 +1,93 @@
+import { Form, FormGroup, Label, Input, Button, FormFeedback } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const initialFormData = {
+    email: "",
+    password: "",
+    terms: false
+}
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.#])[A-Za-z\d@$!%*?&.#]{8,}$/;
+const errorMessages = {
+    email: `Lütfen geçerli bir e-posta adresi giriniz.`,
+    password: `Şifre en az 8 karakter olmalı ve şu özellikleri içermelidir:
+en az bir büyük harf, bir küçük harf, bir sayı ve bir özel karakter.`,
+    terms: "Şartları kabul ediyorum."
+}
+
+export default function Login() {
+    const [form, setForm] = useState(initialFormData);
+    const [isValid, setIsValid] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const emailValid = emailRegex.test(form.email);
+        const passwordValid = passwordRegex.test(form.password);
+        const termsAccepted = form.terms;
+
+        setIsValid(emailValid && passwordValid && termsAccepted);
+    }, [form]);
+
+    function handleChange(event) {
+        const { type, name, value, checked } = event.target;
+        setForm({ ...form, [name]: type === "checkbox" ? checked : value });
+    }
+
+    function handleSubmit(event) {
+        navigate("./Success");
+    }
+
+    return (
+        <>
+            <Form>
+                <FormGroup>
+                    <Label for="exampleEmail">
+                        Email
+                    </Label>
+                    <Input
+                        id="exampleEmail"
+                        name="email"
+                        placeholder="Email giriniz"
+                        type="email"
+                        value={form.email}
+                        invalid={!emailRegex.test(form.email)}
+                        onChange={handleChange}
+                    />
+                    {!emailRegex.test(form.email) && <FormFeedback>
+                        {errorMessages.email}
+                    </FormFeedback>}
+
+                </FormGroup>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Password
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+                        placeholder="Şifre giriniz"
+                        type="password"
+                        value={form.password}
+                        invalid={!passwordRegex.test(form.password)}
+                        onChange={handleChange}
+                    />
+                    {!passwordRegex.test(form.password) && <FormFeedback>
+                        {errorMessages.password}
+                    </FormFeedback>}
+                </FormGroup>
+                <FormGroup check>
+                    <Input type="checkbox" name="terms" checked={form.terms} onChange={handleChange} />
+                    {' '}
+                    <Label check>
+                        {errorMessages.terms}
+                    </Label>
+                </FormGroup>
+                <Button disabled={!isValid} color="primary" onClick={handleSubmit}>
+                    Submit
+                </Button>
+            </Form>
+        </>
+    )
+}
---
+++ src/components/Success.jsx
@@ -0,0 +1,7 @@
+export default function Success() {
+    return (
+        <>
+            <h1>Success Sayfası</h1>
+        </>
+    )
+}
